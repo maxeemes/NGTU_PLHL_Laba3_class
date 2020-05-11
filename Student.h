d4a1d416429422e086213a11acb8450168b7d041
maxemes <max.s.99@yandex.ru>
2020-05-11 18:28:39 +0300

    Creating main student class Created main private fields

--- Student.h
+++ Student.h
@@ -1,1 +1,26 @@
-#pragma once
+#include<string>
+#ifndef STUDENT_H
+#define STUDENT_H
+
+using namespace std;
+struct SubjectMark {
+	string subject;
+	string  mark;
+};
+
+class Student {
+public:
+	Student();
+	Student(string _fio, string _group);
+	Student(string _fio, string _group, int _subjCount, SubjectMark *_marks);
+private:
+	string fio;
+	string group;
+	int subjCount;
+	SubjectMark *marks;
+	bool InitStudentsFromString(string students);
+	bool AddSubjects(string subjects);
+};
+
+
+#endif
